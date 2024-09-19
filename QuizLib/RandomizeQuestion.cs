7e7958aede17e2c4fab594aa1b4289c022c9e8a4
RusandeKodare <o.p.j@hotmail.com>
2024-09-19 11:14:37 +0200

    +RandomizeQuestion class +Three methods

---
+++ QuizLib/RandomizeQuestion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizLib
+{
+    internal class RandomizeQuestion
+    {
+        public Question RandomizeAQuestion()
+        {
+            List<Question> questions = new List<Question>();
+            int rangeOfList = questions.Count();
+            Question randomQuestion = questions[new Random().Next(rangeOfList)];
+            return randomQuestion;
+        }
+        public void RemoveQuestion()
+        {
+            List<Question> questions = new List<Question>();//List of instances with questions
+            int rangeOfList = questions.Count();
+            Question randomQuestion = questions[new Random().Next(rangeOfList)];
+            questions.Remove(randomQuestion);
+        }
+        public void EndlessMode()
+        {
+            List<Question> questions = new List<Question>(); 
+            List<Question> questionsCopy = new List<Question>();
+            int rangeOfList = questionsCopy.Count();
+            int rangeOfListCopy = rangeOfList;   
+            while (questionsCopy.Count > 0)
+            {
+                Question randomQuestion = questionsCopy[new Random().Next(rangeOfList)];
+                questionsCopy.Remove(randomQuestion);
+                rangeOfList--;
+            }
+            if (questionsCopy.Count <= 0)
+            {
+                questionsCopy = questions;
+            }
+        }
+    }
+}
