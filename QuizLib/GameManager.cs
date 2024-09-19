a05f914eb9d9718f2112b801e105b09b2dc15756
grahsp <pontus.grahsp@gmail.com>
2024-09-19 14:31:36 +0200

    Update IO to use generic types

--- QuizLib/GameManager.cs
+++ QuizLib/GameManager.cs
@@ -1,4 +1,6 @@
-﻿namespace QuizLib
+﻿using Classes;
+
+namespace QuizLib
 {
     public enum State
     {
@@ -12,7 +14,6 @@
     {
         private readonly QuestionManager _questionManager;
         private readonly List<Question> _questions;
-        //private readonly IoProvider _ioProvider;
 
         private State _state;
         private int _chances;
@@ -64,7 +65,7 @@
             Console.Clear();
             Question question = _questionManager.GetRandomQuestion();
             Console.WriteLine(question.ToString());
-            var userInput = Console.ReadLine() ?? string.Empty;
+            var userInput = IO.Input<string>();
 
             var result = question.CheckAnswer(userInput);
             Console.WriteLine("Correct: " + result);
