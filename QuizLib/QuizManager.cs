f2b68be80d9d8d7ed1bc57774cd9685986dd0e07
grahsp <pontus.grahsp@gmail.com>
2024-09-19 11:21:18 +0200

    Add basic methods for list management

--- QuizLib/QuizManager.cs
+++ QuizLib/QuizManager.cs
@@ -2,5 +2,31 @@
 {
     public class QuizManager
     {
+        private readonly List<Question> _questions;
+
+        public QuizManager()
+        {
+            
+        }
+
+        public void AddQuestion(Question question)
+        {
+            _questions.Add(question);
+        }
+
+        public void RemoveQuestion(Question question)
+        {
+            _questions.Remove(question);
+        }
+
+        public void RemoveQuestion(int index)
+        {
+            _questions.Remove(_questions[index]);
+        }
+
+        public Question GetQuestion(int index)
+        {
+            return _questions[index];
+        }
     }
 }
